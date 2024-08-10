72a32caef45791c0768231a78835cafd8aa244af
TeachDian <philiptenibas@gmail.com>
2024-08-10 10:19:05 +0800

    s

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
 
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './components/styles/backgroundNoise.css';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="bg"></div>
+      {/* Your other components go here */}
     </div>
   );
 }
 
 export default App;
+
+
